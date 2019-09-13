3c54ca95363bd2f27e13421fbda54ac7597802c6
Vasilii Pochkenko <pochkaenkov@yandex.ru>
2019-09-14 00:35:26 +0700

    feat: Print exec file name.

--- SortImages/main.cpp
+++ SortImages/main.cpp
@@ -1,3 +1,4 @@
+#include <QDebug>
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 
@@ -6,6 +7,8 @@ int main(int argc, char *argv[])
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
     QGuiApplication app(argc, argv);
+
+    qDebug()<<QCoreApplication::applicationFilePath();
 
     QQmlApplicationEngine engine;
     const QUrl url(QStringLiteral("qrc:/main.qml"));
