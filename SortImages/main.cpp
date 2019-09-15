a092c469adb977ae5e3e4cbb29c0c73e08e5d8a3
Vasilii Pochkenko <pochkaenkov@yandex.ru>
2019-09-15 13:37:42 +0700

    feat: Bind the images classificator to GUI.

--- SortImages/main.cpp
+++ SortImages/main.cpp
@@ -1,29 +1,33 @@
 #include <QDebug>
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QQmlContext>
 #include <QSettings>
+#include "imageclassifikator.h"
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+	QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
-    QGuiApplication app(argc, argv);
+	QGuiApplication app(argc, argv);
 
-    qDebug()<<QCoreApplication::applicationFilePath();
-    QSettings config(QCoreApplication::applicationFilePath() + ".ini", QSettings::IniFormat);
-    config.setIniCodec("UTF-8");
-    qDebug()<<config.value("SourceDir");
-    qDebug()<<config.value("TrashDir");
-    qDebug()<<config.value("ValidDir");
+	qDebug()<<QCoreApplication::applicationFilePath();
+	QSettings config(QCoreApplication::applicationFilePath() + ".ini", QSettings::IniFormat);
+	config.setIniCodec("UTF-8");
+	qDebug()<<config.value("SourceDir");
+	qDebug()<<config.value("TrashDir");
+	qDebug()<<config.value("ValidDir");
 
-    QQmlApplicationEngine engine;
-    const QUrl url(QStringLiteral("qrc:/main.qml"));
-    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
-                     &app, [url](QObject *obj, const QUrl &objUrl) {
-        if (!obj && url == objUrl)
-            QCoreApplication::exit(-1);
-    }, Qt::QueuedConnection);
-    engine.load(url);
+	QQmlApplicationEngine engine;
+	ImageClassifikator imageClassifikator(&app);
+	engine.rootContext()->setContextProperty("imageClassifikator", &imageClassifikator);
+	const QUrl url(QStringLiteral("qrc:/main.qml"));
+	QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+	                 &app, [url](QObject *obj, const QUrl &objUrl) {
+		if (!obj && url == objUrl)
+			QCoreApplication::exit(-1);
+	}, Qt::QueuedConnection);
+	engine.load(url);
 
-    return app.exec();
+	return app.exec();
 }
